5a7ac10dae14455ceff3619fa721d83aba94ebb3
Mark Voorberg <mark@binaryops.ca>
2021-11-19 08:04:05 -0800

    minor bug-fixes

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function TransomAtlasAccesslist() {
 
     const publicKey = atlasDefn.publicKey;
     const privateKey = atlasDefn.privateKey;
-    const appName = atlasDefn.name || transomConfig.name || "TransomJS";
+    const appName = atlasDefn.name || (transomConfig.name || "TransomJS") + `(${process.env.NODE_ENV})`;
     const projectId = atlasDefn.projectId;
     const barePublicIpUrl =
       atlasDefn.barePublicIpUrl || "https://api.ipify.org/";
 
-    const options = {
+    const atlasOpts = {
       publicKey,
       privateKey,
       appName,
@@ -25,7 +25,7 @@ function TransomAtlasAccesslist() {
       barePublicIpUrl: barePublicIpUrl,
     };
 
-    return updateAtlas(options);
+    return updateAtlas(atlasOpts);
   };
 }
 
